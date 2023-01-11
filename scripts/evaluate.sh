3372e0c8af49a43324cb916d1f686b24d4c96933
Márton Kardos <power.up1163@gmail.com>
2023-01-11 14:26:14 +0100

    New pushing and evaluation scripts

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,1 +1,14 @@
-# TODO
+# Assigning command line arguments to variables
+MODEL_NAME = $1
+PACKAGE_VERSION = $2
+
+source environments/training/bin/activate
+for CORPUS in "joint" "perseus" "proiel"
+do
+    mkdir -p "metrics/$MODEL_NAME/$PACKAGE_VERSION/$CORPUS"
+    for WHICH in "best" "last"
+    do
+        python3 -m spacy evaluate "training/$MODEL_NAME/model-$WHICH" corpus/$CORPUS/test.spacy --output "metrics/$MODEL_NAME/$PACKAGE_VERSION/$CORPUS/$WHICH.json"
+    done
+done
+deactivate
