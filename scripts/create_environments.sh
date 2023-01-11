225e087fbdb6f0f834324700c8c8b83e992f6ae9
Márton Kardos <power.up1163@gmail.com>
2023-01-11 14:00:15 +0100

    FOr loop for creating environments

--- scripts/create_environments.sh
+++ scripts/create_environments.sh
@@ -2,18 +2,10 @@
 rm -rf environments/
 # Making folder for environments
 mkdir -p environments
-# Creating environment for preprocessing
-python3 -m venv environments/preprocessing
-source environments/preprocessing/bin/activate
-pip install -r requirements_preprocessing.txt
-deactivate
-# Creating environment for training
-python3 -m venv environments/training
-source environments/training/bin/activate
-pip install -r requirements_training.txt
-deactivate
-# Creating environment for packaging
-python3 -m venv environments/packaging
-source environments/packaging/bin/activate
-pip install -r requirements_packaging.txt
-deactivate
+for ENVIRONMENT in "preprocessing" "training" "packaging"
+do
+    python3 -m venv environments/$ENVIRONMENT
+    source environments/$ENVIRONMENT/bin/activate
+    pip install -r "requirements_$ENVIRONMENT.txt"
+    deactivate
+done
