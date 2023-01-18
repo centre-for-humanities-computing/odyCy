77401c7a8bc7cc8f8cc9009c2a74b8ed2b266ddd
jankounchained <42962106+jankounchained@users.noreply.github.com>
2023-01-18 18:53:26 +0100

    evaluation for hub models

---
+++ scripts/evaluate_checkpoints.sh
@@ -0,0 +1,11 @@
+# Assigning command line arguments to variables
+MODEL_NAME=$1
+PACKAGE_VERSION=$2
+
+source environments/training/bin/activate
+echo "Using $VIRTUAL_ENV"
+for CORPUS in "joint" "perseus" "proiel"
+do
+    mkdir -p "metrics/$MODEL_NAME/$PACKAGE_VERSION/$CORPUS"
+    spacy evaluate "$MODEL_NAME" corpus/$CORPUS/test.spacy --output "metrics/$MODEL_NAME/$PACKAGE_VERSION/$CORPUS/hub_version.json"
+done
--- scripts/get_model_checkpoints.sh
+++ scripts/get_model_checkpoints.sh
@@ -1,6 +1,10 @@
 # use the training venv
 source environments/training/bin/activate
 
+# clean 
+pip uninstall grc_homercy_treebanks_trf -y
+pip uninstall grc_homercy_treebanks_sm -y
+
 # transformer model
 pip install https://huggingface.co/janko/grc_homercy_treebanks_trf/resolve/main/grc_homercy_treebanks_trf-any-py3-none-any.whl
 # cpu model
