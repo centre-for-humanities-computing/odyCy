79a4f37a370fac3cb1aeff01e4d382c6096dbf7c
jankounchained <42962106+jankounchained@users.noreply.github.com>
2023-01-19 13:06:44 +0100

    imported spacy models require their own environement

--- scripts/evaluate/evaluate_jacobo.sh
+++ scripts/evaluate/evaluate_jacobo.sh
@@ -1,4 +1,16 @@
-source environments/training/bin/activate
+# create a new venv
+python3 -m venv environments/eval_jacobo
+source environments/eval_jacobo/bin/activate
+
+# fetch jacobo's models
+# clean existing instalation
+pip uninstall grc_ud_proiel_trf -y
+pip uninstall grc_ud_perseus_trf -y
+
+# install models
+pip install https://huggingface.co/Jacobo/grc_ud_proiel_trf/resolve/main/grc_ud_proiel_trf-any-py3-none-any.whl
+pip install https://huggingface.co/Jacobo/grc_ud_perseus_trf/resolve/main/grc_ud_perseus_trf-any-py3-none-any.whl
+
 echo "Using $VIRTUAL_ENV"
 for MODEL_NAME in "grc_ud_proiel_trf" "grc_ud_perseus_trf"
     do
