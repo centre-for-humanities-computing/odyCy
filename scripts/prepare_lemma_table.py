a81aaa25057ec3a03d08f25e90bb2533991927e0
Márton Kardos <power.up1163@gmail.com>
2023-01-25 13:02:00 +0100

    Refactoring: Moved conll utilities to utils

--- scripts/prepare_lemma_table.py
+++ scripts/prepare_lemma_table.py
@@ -1,75 +1,12 @@
 """Creates a table of lemmas with form, upos, morhpology and
 frequency for our custom lemmatizer."""
 
-from typing import Dict
-from io import StringIO
 import json
 
-import pandas as pd
-
-# Columns in a CONLL-U file
-CONLL_COLUMNS = [
-    "word_id",
-    "form",
-    "lemma",
-    "upos",
-    "xpos",
-    "feats",
-    "head",
-    "deprel",
-    "deps",
-    "misc",
-]
+from utils.conll import features_to_dict, load_conllu, POSSIBLE_FEATURES
 
 TREEBANKS_PATH = "assets/treebanks/joint/train.conllu"
 LEMMA_TABLE_PATH = "assets/lemmas/lemma_table.json"
-POSSIBLE_FEATURES = [
-    "Tense",
-    "VerbForm",
-    "Voice",
-    "Case",
-    "Gender",
-    "Number",
-    "Degree",
-    "Mood",
-    "Person",
-    "Aspect",
-    "Definite",
-    "PronType",
-    "Polarity",
-    "Poss",
-    "Reflex",
-]
-
-
-def load_conllu(path: str) -> pd.DataFrame:
-    """Reads a CONLL-U file into a Dataframe"""
-    with open(path) as conllu_file:
-        lines = []
-        # I only append lines from the file that are not comments.
-        for line in conllu_file:
-            if not line.startswith("#"):
-                lines.append(line)
-    # Joining the lines
-    conllu_text = "".join(lines)
-    # Turning it into a stream so that pandas can read it as a file.
-    text_stream = StringIO(conllu_text)
-    # Reading conllu files to a dataframe
-    df = pd.read_csv(text_stream, sep="\t", names=CONLL_COLUMNS)
-    return df
-
-
-def features_to_dict(feature_string: str) -> Dict[str, str]:
-    """Converts a string of morphological features to a
-    dictionary."""
-    features = dict()
-    if feature_string == "_":
-        return features
-    declarations = feature_string.split("|")
-    for declaration in declarations:
-        feature, value = declaration.split("=")
-        features[str(feature)] = str(value)
-    return features
 
 
 def main() -> None:
