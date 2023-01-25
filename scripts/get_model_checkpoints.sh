23c77d8e0565136a4708fa36ba94afae45231b8c
jankounchained <42962106+jankounchained@users.noreply.github.com>
2023-01-25 14:20:01 +0100

    preparing for downloading final models from hf hub

--- scripts/get_model_checkpoints.sh
+++ scripts/get_model_checkpoints.sh
@@ -4,7 +4,11 @@ source environments/training/bin/activate
 # clean our models
 pip uninstall grc_homercy_treebanks_trf -y
 pip uninstall grc_homercy_treebanks_sm -y
+pip uninstall grc_dep_treebanks_trf -y
+pip uninstall grc_dep_treebanks_sm -y
 
 # install our models
 pip install https://huggingface.co/janko/grc_homercy_treebanks_trf/resolve/main/grc_homercy_treebanks_trf-any-py3-none-any.whl
 pip install https://huggingface.co/kardosdrur/grc_homercy_treebanks_sm/resolve/main/grc_homercy_treebanks_sm-any-py3-none-any.whl
+
+# TODO install final models
