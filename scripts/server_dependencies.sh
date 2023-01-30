8f16a670e367a25f17ff12c27a45c94b62013c88
jankounchained <honza.anfas@gmail.com>
2023-01-30 11:51:47 +0100

    fix: add zip

--- scripts/server_dependencies.sh
+++ scripts/server_dependencies.sh
@@ -4,6 +4,6 @@ sudo apt update -y
 # nvidia drivers
 sudo apt install nvidia-headless-460 nvidia-utils-460 -y
 # general util
-sudo apt install unzip git-lfs -y
+sudo apt install zip unzip git-lfs -y
 # python util
 sudo apt install python3-pip python3.8-venv -y
