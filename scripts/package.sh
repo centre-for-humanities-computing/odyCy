096aa1ce01348580e210ce7ab48feb30534c1c73
jankounchained <honza.anfas@gmail.com>
2023-01-30 13:13:47 +0100

    rework packaging & split it from pushing

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,12 +1,30 @@
 MODEL_NAME=$1
 PACKAGE_NAME=$2
 PACKAGE_VERSION=$3
+CUSTOM_COMPONENT=$4
 
 source environments/training/bin/activate
 
 mkdir -p packages
-python3 -m spacy package "training/$MODEL_NAME/model-best" "packages/" \
-    --build wheel \
-    --code "custom_components/lemmatizer.py" \
-    --create-meta \
-    --version $PACKAGE_VERSION
+
+args=()
+# input / output
+args+=( "training/$MODEL_NAME/model-best" )
+args+=( "packages/" )
+# build style
+args+=( "--build" )
+args+=( "wheel" )
+# metadata
+args+=( "--create-meta" )
+args+=( "--name" ) # override meta package name
+args+=( "$PACKAGE_NAME" )
+args+=( "--version" ) # override meta with version
+args+=( "$PACKAGE_VERSION" )
+
+if [ "$CUSTOM_COMPONENT" == "frequency_lemmatizer" ]
+then
+    args+=( "--code" ) 
+    args+=( "custom_components/lemmatizer.py" )
+fi
+
+python3 -m spacy package "${args[@]}"
