ffd710f13c5eecc87c1c300b9b58ce8becf4d439
jankounchained <honza.anfas@gmail.com>
2023-01-31 09:51:44 +0100

    final models to be installed, freq_lemm component included in all models

--- scripts/dependencies/get_homercy_checkpoints.sh
+++ scripts/dependencies/get_homercy_checkpoints.sh
@@ -9,6 +9,8 @@ pip uninstall grc_dep_treebanks_sm -y
 
 # install final models
 pip install https://huggingface.co/janko/grc_homercy_treebanks_trf/resolve/main/grc_homercy_treebanks_trf-any-py3-none-any.whl
+pip install https://huggingface.co/janko/grc_homercy_treebanks_sm/resolve/main/grc_homercy_treebanks_sm-any-py3-none-any.whl
 
 # install dep models
 pip install https://huggingface.co/janko/grc_dep_treebanks_trf/resolve/main/grc_dep_treebanks_trf-any-py3-none-any.whl
+pip install https://huggingface.co/janko/grc_dep_treebanks_sm/resolve/main/grc_dep_treebanks_sm-any-py3-none-any.whl
